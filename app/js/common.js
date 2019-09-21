9621c7782dc8dcdf6fec56c0bcf825cd5f41ee95
Nazar Lesyuk <pronaza@gmail.com>
2019-09-21 23:20:23 +0300

    some

--- app/js/common.js
+++ app/js/common.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
 
 	$(document).on('click', function(e) {
 		var isHas = e.target.classList.contains('form__add');
-		console.log(this)
-		console.log(e)
-		console.log(isHas)
+		if( isHas ) return;
 
-		if( $(this).hasClass("form__add") ) {
+		if( $(e.target).hasClass("form__add") ) {
 			alert(1)
+		} else {
+
 		}
+		
 	});
 
 //end ready

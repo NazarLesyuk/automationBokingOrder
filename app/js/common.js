f083b62f1e611a15076f87696918abcc087349e6
Nazar Lesyuk <pronaza@gmail.com>
2019-09-22 23:36:30 +0300

    ss

--- app/js/common.js
+++ app/js/common.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
 
 	$(document).on('click', function(e) {
 		var isHas = e.target.classList.contains('form__add');
-		if( isHas ) return;
+		// if( isHas ) return;
 
 		if( $(e.target).hasClass("form__add") ) {
-			alert(1)
+			// var parent = $(e.target).parent('.fieldset__cont').get(0);
+			// $("#mainform").appendChild( $(".fieldset__cont")[0] );
+			// $("#mainform").append( 'wtf' );
+			var dom = document.querySelectorAll(".fieldset__cont")[0];
+			console.log(dom);
+			// document.getElementById("mainform").appendChild( dom ); 
+			var test = dom.cloneNode(true);
+			document.querySelector("#mainform").appendChild( test ); 
 		} else {
 
 		}
